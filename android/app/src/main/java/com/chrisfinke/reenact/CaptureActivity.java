68c2c9e2855094f9c78d754bbb82aa0876760d72
Christopher Finke <cfinke@gmail.com>
2015-11-14 03:15:16 +0000

    Orientation is set based on the original photo orientation.

--- android/app/src/main/java/com/chrisfinke/reenact/CaptureActivity.java
+++ android/app/src/main/java/com/chrisfinke/reenact/CaptureActivity.java
@@ -2,6 +2,7 @@ package com.chrisfinke.reenact;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,6 +16,8 @@ import java.io.InputStream;
 public class CaptureActivity extends Activity {
     private Uri originalPhotoUri;
     private String LOG_TAG = "reenact";
+
+    public String orientation = "portrait";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,5 +48,18 @@ public class CaptureActivity extends Activity {
             }
         }
 
+        int imageHeight = imageView.getDrawable().getIntrinsicHeight();
+        int imageWidth = imageView.getDrawable().getIntrinsicWidth();
+
+        if ( imageWidth > imageHeight ) {
+            orientation = "landscape";
+        }
+
+        if ( orientation.equals("portrait") ) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+        else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
     }
 }
