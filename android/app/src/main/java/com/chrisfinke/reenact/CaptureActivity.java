1d37854383c00316cfda1dbcbfcdff915ac22ce0
Christopher Finke <cfinke@gmail.com>
2015-11-14 03:12:55 +0000

    Capture activity displays the original photo.

--- android/app/src/main/java/com/chrisfinke/reenact/CaptureActivity.java
+++ android/app/src/main/java/com/chrisfinke/reenact/CaptureActivity.java
@@ -2,9 +2,15 @@ package com.chrisfinke.reenact;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class CaptureActivity extends Activity {
     private Uri originalPhotoUri;
@@ -19,5 +25,25 @@ public class CaptureActivity extends Activity {
         originalPhotoUri = intent.getParcelableExtra(IntroActivity.ORIGINAL_PHOTO_PATH);
 
         Log.d(LOG_TAG, "Received original photo URI: " + originalPhotoUri.toString());
+
+        ImageView imageView = (ImageView) findViewById(R.id.original_image);
+
+        InputStream imageStream = null;
+
+        try {
+            imageStream = getContentResolver().openInputStream(originalPhotoUri);
+            imageView.setImageBitmap(BitmapFactory.decodeStream(imageStream));
+        } catch (FileNotFoundException e) {
+            // @todo Deal with this.
+        } finally {
+            if (imageStream != null) {
+                try {
+                    imageStream.close();
+                } catch (IOException e) {
+                    // Ignorable?
+                }
+            }
+        }
+
     }
 }
