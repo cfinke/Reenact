a627014e799ecd2a42e7f90bb008e3306e81cf88
Christopher Finke <cfinke@gmail.com>
2015-11-14 18:59:37 +0000

    Displaying portrait photos as the same height in the confirm screen.

--- android/app/src/main/java/com/chrisfinke/reenact/ConfirmActivity.java
+++ android/app/src/main/java/com/chrisfinke/reenact/ConfirmActivity.java
@@ -3,16 +3,25 @@ package com.chrisfinke.reenact;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ConfirmActivity extends Activity {
     private Uri originalPhotoUri;
@@ -51,6 +60,48 @@ public class ConfirmActivity extends Activity {
 
         Bitmap bMap = BitmapFactory.decodeByteArray(newPhotoBytes, 0, newPhotoBytes.length);
         imageViewNow.setImageBitmap(bMap);
+
+        int oldImageHeight = imageViewThen.getDrawable().getIntrinsicHeight();
+        int newImageHeight = imageViewNow.getDrawable().getIntrinsicHeight();
+
+        int oldImageWidth = imageViewThen.getDrawable().getIntrinsicWidth();
+        int newImageWidth = imageViewNow.getDrawable().getIntrinsicWidth();
+
+        int adjustedOldImageHeight = Math.round((float) oldImageWidth / newImageWidth * oldImageHeight);
+        int adjustedNewImageHeight = Math.round((float) oldImageWidth / newImageWidth * newImageHeight);
+
+        Log.d(Constants.LOG_TAG, "oldImageHeight: " + oldImageHeight);
+        Log.d(Constants.LOG_TAG, "newImageHeight: " + newImageHeight);
+
+        if ( newImageWidth < newImageHeight ) {
+            // Portrait.
+            int shorterHeight = Math.min( oldImageHeight, newImageHeight );
+
+            int sameHeightOldImageHeight = ( shorterHeight / oldImageHeight ) * oldImageHeight;
+            int sameHeightNewImageHeight = ( shorterHeight / newImageHeight ) * newImageHeight;
+
+            int sameHeightOldImageWidth = Math.round( ( (float) shorterHeight / oldImageHeight ) * oldImageWidth);
+            int sameHeightNewImageWidth = Math.round( ( (float) shorterHeight / newImageHeight ) * newImageWidth);
+
+            float oldImageWeight = ( (float) sameHeightOldImageWidth ) / ( sameHeightOldImageWidth + sameHeightNewImageWidth );
+            float newImageWeight = ( (float) sameHeightNewImageWidth ) / ( sameHeightOldImageWidth + sameHeightNewImageWidth );
+
+            Log.d(Constants.LOG_TAG, "sameHeightOldImageWidth: " + sameHeightOldImageWidth);
+            Log.d(Constants.LOG_TAG, "sameHeightNewImageWidth: " + sameHeightNewImageWidth);
+
+            Log.d(Constants.LOG_TAG, "oldImageWeight: " + oldImageWeight);
+            Log.d(Constants.LOG_TAG, "newImageWeight: " + newImageWeight);
+
+            LinearLayout.LayoutParams oldParams = (LinearLayout.LayoutParams) imageViewThen.getLayoutParams();
+            oldParams.weight = oldImageWeight;
+            imageViewThen.setLayoutParams( oldParams );
+
+            LinearLayout.LayoutParams newParams = (LinearLayout.LayoutParams) imageViewNow.getLayoutParams();
+            newParams.weight = newImageWeight;
+            imageViewNow.setLayoutParams( newParams );
+        }
+        else {
+        }
     }
 
     public void goBack(View view) {
