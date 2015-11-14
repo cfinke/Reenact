582a6efac3676678458ef56b7bb4c4b20c435953
Christopher Finke <cfinke@gmail.com>
2015-11-14 02:12:12 +0000

    Android intro screen. No functionality yet.

---
+++ android/app/src/main/java/com/chrisfinke/reenact/IntroActivity.java
@@ -0,0 +1,13 @@
+package com.chrisfinke.reenact;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IntroActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
