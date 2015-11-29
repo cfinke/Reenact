30272ec740c269801c33cfad079c801cbc8fb8dd
Christopher Finke <cfinke@gmail.com>
2015-11-29 03:25:10 +0000

    Share Controller

---
+++ ios/Reenact/Reenact/ShareController.swift
@@ -0,0 +1,28 @@
+//
+//  ShareController.swift
+//  Reenact
+//
+//  Created by Christopher Finke on 11/25/15.
+//  Copyright © 2015 Christopher Finke. All rights reserved.
+//
+
+import UIKit
+
+class ShareController: ReenactControllerBase {
+    // MARK: Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Actions
+    
+    // MARK: Delegates
+    
+}
+
